1528d30485b215d696bcd78688c120b0fb8336b6
dylansebastianm <dylan.sebastianmte@gmail.com>
2025-12-11 22:07:59 -0300

    test

--- src/pages/api/payments.js
+++ src/pages/api/payments.js
@@ -20,7 +20,7 @@ const NOTIFICATION_URL =
   'https://services.leadconnectorhq.com/hooks/2QXih6adBOqlYCUJlZAp/webhook-trigger/844745bf-b769-4c6b-88a1-92d05ad9865e'
 const BACK_URL = 'https://www.worksfound.com/checkout'
 const SUCCESS_URL = 'https://www.worksfound.com/thanks'
-const AMOUNT = 140
+const AMOUNT = 1
 const CURRENCY = 'USD'
 
 /**
